9d85eab8b6db60fba0bc411714d36a1b77a01bbf
Kamal Mostafa <kamal@whence.com>
2020-12-11 15:22:24 -0800

    add libsndio dependency

--- install-dependencies-linux.sh
+++ install-dependencies-linux.sh
@@ -4,5 +4,5 @@ set -x
 set -o pipefail
 
 sudo apt update
-sudo apt install libsndfile1-dev fftw3-dev libasound2-dev libpulse-dev
+sudo apt install libsndfile1-dev fftw3-dev libasound2-dev libpulse-dev libsndio-dev
 sudo apt install pkg-config automake
