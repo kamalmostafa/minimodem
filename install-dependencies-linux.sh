fe0082838ef114a0df0eca5b0122c2d00d04d762
Jannis R <mail@jannisr.de>
2021-01-06 22:01:43 +0100

    Travis CI -> GitHub Actions CI 💚

--- install-dependencies-linux.sh
+++ install-dependencies-linux.sh
@@ -3,6 +3,6 @@ set -e
 set -x
 set -o pipefail
 
-sudo apt update
-sudo apt install libsndfile1-dev fftw3-dev libasound2-dev libpulse-dev libsndio-dev
-sudo apt install pkg-config automake
+sudo $(which apt) update
+sudo $(which apt) install libsndfile1-dev fftw3-dev libasound2-dev libpulse-dev libsndio-dev
+sudo $(which apt) install pkg-config automake
--- install-dependencies-macos.sh
+++ install-dependencies-macos.sh
@@ -3,5 +3,5 @@ set -e
 set -x
 set -o pipefail
 
-brew install libsndfile fftw
+brew install libsndfile fftw autoconf libtool
 brew install automake || brew upgrade automake
